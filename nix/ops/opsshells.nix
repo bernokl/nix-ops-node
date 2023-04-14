b5c0fd2d6c99cd1b396c797127dd93f581f4ad7e
Berno <bernokl@gmail.com>
2023-04-15 00:12:17 +1200

    Restructure repo

--- nix/ops/opsshells.nix
+++ nix/ops/opsshells.nix
@@ -6,16 +6,12 @@ let
 
 in {
   dev = (lib.dev.mkShell {
-
     name = "example-devshell";
-
     imports = [ std.devshellProfiles.default ];
-
     packages = [
       cell.toolchain.rust.stable.latest.default
       nix-serve
     ];
-
     commands = [
       {
         name = "tests";
@@ -23,7 +19,29 @@ in {
         help = "run the unit tests";
         category = "Testing";
       }
+      {
+        name = "serve";
+        command = "${nix-serve}/bin/nix-serve --port 8080";
+        help = "run the unit tests";
+        category = "Testing";
+      }
 
+    ];
+  }) // { meta.description = "General development shell with default yumi environment."; }; 
+  ops = (lib.dev.mkShell {
+    name = "example-devshell";
+    imports = [ std.devshellProfiles.default ];
+    packages = [
+      cell.toolchain.rust.stable.latest.default
+      nix-serve
+    ];
+    commands = [
+      {
+        name = "tests";
+        command = "cargo test";
+        help = "run the unit tests";
+        category = "Testing";
+      }
       {
         name = "serve";
         command = "${nix-serve}/bin/nix-serve --port 8080";
